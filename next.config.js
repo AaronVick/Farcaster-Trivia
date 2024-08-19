09d30ba4c1727d4056cdf682b4b3e9fb45635ba6
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-19 16:31:39 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/triviaFrame',
-        destination: '/api/triviaFrame',
-      },
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ]
-  },
   async headers() {
     return [
       {
@@ -24,14 +12,5 @@ module.exports = {
         ],
       },
     ]
-  },
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: 'secret',
-    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: '/static',
   },
 }
